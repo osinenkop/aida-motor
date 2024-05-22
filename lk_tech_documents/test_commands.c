7328398ea1fc21370154725e42f0646174aefa92
sinamoghimi <sinamoghimi73@gmail.com>
2024-05-22 19:12:29 +0300

    Documents

---
+++ lk_tech_documents/test_commands.c
@@ -0,0 +1,334 @@
+/* Multi Loop*/    
+
+/* Multi position  loopⅠ  360000° 1000turn*/     
+3E A3 01 08 EA 00 51 25 02 00 00 00 00 78
+
+/* Multi position  loopⅠ  36000° 100turn */   
+3E A3 01 08 EA 80 EE 36 00 00 00 00 00 A4
+
+/* Multi position  loopⅠ  360° */    
+3E A3 01 08 EA A0 8C 00 00 00 00 00 00 2C
+
+/* Multi position  loopⅠ  180° */    
+3E A3 01 08 EA 50 46 00 00 00 00 00 00 96
+
+/* Multi position  loopⅠ  110° */
+3E A3 01 08 EA F8 2A 00 00 00 00 00 00 22
+
+/* Multi position  loopⅠ  90° */
+3E A3 01 08 EA 28 23 00 00 00 00 00 00 4B
+
+/* Multi position loopⅠ  -90° */
+3E A3 01 08 EA D7 DC FF FF FF FF FF FF AD
+
+/* Multi position  loopⅠ 1.0° */
+3E A3 01 08 EA 64 00 00 00 00 00 00 00 64
+
+/* Multi position loopⅠ   0.5° */
+3E A3 01 08 EA 32 00 00 00 00 00 00 00 32
+
+/* Multi position  loopⅠ  0.1° */
+3E A3 01 08 EA 0A 00 00 00 00 00 00 00 0A
+
+/* Multi position  loopⅠ  0° */
+3E A3 01 08 EA 00 00 00 00 00 00 00 00 00
+/********************************************************************************************************************************************/
+/* Multi position loopⅡ  360° ，Speed 10*/
+3E A4 01 0C EF A0 8C 00 00 00 00 00 00 E8 03 00 00 17
+
+/* Multi position  loopⅡ   360°， Speed 90*/
+3E A4 01 0C EF A0 8C 00 00 00 00 00 00 28 23 00 00 77
+
+/* Multi position loopⅡ   180° ，Speed 90 */
+3E A4 01 0C EF 50 46 00 00 00 00 00 00 28 23 00 00 E1
+
+/* Multi position loopⅡ   90° ，Speed 90 */
+3E A4 01 0C EF 28 23 00 00 00 00 00 00 28 23 00 00 96
+
+/* Multi position loopⅡ   0° ，Speed 90 */
+3E A4 01 0C EF 00 00 00 00 00 00 00 00 28 23 00 00 4B
+
+/* Multi position loopⅡ   3600° ，Speed 360*/
+3E A4 01 0C EF 40 7E 05 00 00 00 00 00 A0 8C 00 00 EF
+
+/* Multi position loopⅡ   0° ，Speed 360 */
+3E A4 01 0C EF 00 00 00 00 00 00 00 00 A0 8C 00 00 2C
+
+
+/********************************************************************************************************************************************/
+/* Single loop */
+
+/* Single position loopⅠ (forward)359.99° */
+3E A5 01 04 E8 00 9F 8C 00 2B
+
+/* Single position loopⅠ (Forward)315° */
+3E A5 01 04 E8 00 0C 7B 00 87
+
+/* Single position loopⅠ (Forward)270° */
+3E A5 01 04 E8 00 78 69 00 E1
+
+/* Single position loopⅠ (Forward)225° */
+3E A5 01 04 E8 00 E4 57 00 3B
+
+/* Single position loopⅠ (Forward)180° */
+3E A5 01 04 E8 00 50 46 00 96
+
+/* Single position loopⅠ (Forward)135° */
+3E A5 01 04 E8 00 BC 34 00 F0
+
+/* Single position loopⅠ (Forward)90° */
+3E A5 01 04 E8 00 28 23 00 4B
+
+/* Single position loopⅠ (Forward)45° */
+3E A5 01 04 E8 00 94 11 00 A5
+
+/* Single position loopⅠ (Forward)0° */
+3E A5 01 04 E8 00 00 00 00 00
+
+/* Single position loopⅠ (reverse)359.99° */
+3E A5 01 04 E8 01 9F 8C 00 2C
+
+/* Single position loopⅠ (reverse)315° */
+3E A5 01 04 E8 01 0C 7B 00 88
+
+/* Single position loopⅠ (reverse)270° */
+3E A5 01 04 E8 01 78 69 00 E2
+
+/* Single position loopⅠ (reverse)225° */
+3E A5 01 04 E8 01 E4 57 00 3C
+
+/* Single position loopⅠ (reverse)180° */
+3E A5 01 04 E8 01 50 46 00 97
+
+/* Single position loopⅠ (reverse)135° */
+3E A5 01 04 E8 01 BC 34 00 F1
+
+/* Single position loopⅠ (reverse)90° */
+3E A5 01 04 E8 01 28 23 00 4C
+
+/* Single position loopⅠ (reverse)45° */
+3E A5 01 04 E8 01 94 11 00 A6
+
+/* Single position loopⅠ (reverse)0° */
+3E A5 01 04 E8 01 00 00 00 01
+/********************************************************************************************************************************************/
+/* Single position loop Ⅱ (Forward)180°，Speed 10*/
+3E A6 01 08 ED 00 50 46 00 E8 03 00 00 81
+
+/* Single position loop Ⅱ (Forward)90°，Speed 90*/
+3E A6 01 08 ED 00 28 23 00 28 23 00 00 96
+
+/* Single position loop Ⅱ (Forward)0°，Speed 10*/
+3E A6 01 08 ED 00 00 00 00 E8 03 00 00 EB
+
+/* 单Single position loop Ⅱ (reverse)315°，Speed 180*/
+3E A6 01 08 ED 01 0C 7B 00 50 46 00 00 1E
+
+/* Single position loop Ⅱ (reverse)45°，Speed 45*/
+3E A6 01 08 ED 01 94 11 00 94 11 00 00 4B
+
+/* Single position loop Ⅱ (reverse)45°，Speed 45*/
+3E A6 01 08 ED 01 00 00 00 94 11 00 00 A6
+
+/* Single position loop Ⅱ (Forward)90°，Speed 90*/
+3E A6 03 08 EF 00 28 23 00 28 23 00 00 96
+
+/* Single position loop Ⅱ (Forward)5°，Speed 90*/
+3E A6 03 08 EF 00 F4 01 00 28 23 00 00 40
+
+/* Single position loop Ⅱ (reverse)0°，Speed 45*/
+3E A6 03 08 EF 01 00 00 00 94 11 00 00 A6
+
+/* Single position loop Ⅱ (reverse)45°，Speed 45*/
+3E A6 03 08 EF 01 94 11 00 94 11 00 00 4B
+
+/********************************************************************************************************************************************/
+/* Speed Loop*/
+
+/* Speed Loop  2880dps */
+3E A2 01 04 E5 00 65 04 00 69
+
+/* Speed Loop  720dps */
+3E A2 01 04 E5 40 19 01 00 5A
+
+/* Speed Loop  360dps */
+3E A2 01 04 E5 A0 8C 00 00 2C
+
+/* Speed Loop  200dps */
+3E A2 01 04 E5 20 4E 00 00 6E
+
+/* Speed Loop  180dps */
+3E A2 01 04 E5 50 46 00 00 96
+
+/* Speed Loop  90dps */
+3E A2 01 04 E5 28 23 00 00 4B
+
+/* Speed Loop  45dps */
+3E A2 01 04 E5 94 11 00 00 4B
+
+/* Speed Loop  0dps */
+3E A2 01 04 E5 00 00 00 00 00
+
+/* Speed Loop  -90dps */
+3E A2 01 04 E5 D8 DC FF FF B2
+
+/* Speed Loop  1.8dps */
+3E A2 01 04 E5 B4 00 00 00 B4
+
+/* Speed Loop  5dps */
+3E A2 01 04 E5 F4 01 00 00 F5
+
+/********************************************************************************************************************************************/
+/* Torque Loop */
+
+/* Torque Loop  3500 */
+3E A1 01 02 E2 AC 0D B9
+
+/* Torque Loop  3000 */
+3E A1 01 02 E2 B8 0B C3
+
+/* Torque Loop  2500 */
+3E A1 01 02 E2 C4 09 CD
+
+/* Torque Loop  2000 */
+3E A1 01 02 E2 D0 07 D7
+
+/* Torque Loop  1600 */
+3E A1 01 02 E2 40 06 46
+
+/* Torque Loop  1500 */
+3E A1 01 02 E2 DC 05 E1
+
+/* Torque Loop  1400 */
+3E A1 01 02 E2 78 05 7D
+
+/* Torque Loop  1300 */
+3E A1 01 02 E2 14 05 19
+
+/* Torque Loop  1200 */
+3E A1 01 02 E2 B0 04 B4
+
+/*Torque Loop  1100 */
+3E A1 01 02 E2 4C 04 50
+
+/* Torque Loop  1000 */
+3E A1 01 02 E2 E8 03 EB
+
+/* Torque Loop  900 */
+3E A1 01 02 E2 84 03 87
+
+/* Torque Loop  800 */
+3E A1 01 02 E2 20 03 23
+
+/* Torque Loop  700 */
+3E A1 01 02 E2 BC 02 BE
+
+/* Torque Loop  600 */
+3E A1 01 02 E2 58 02 5A
+
+/* Torque Loop  500 */
+3E A1 01 02 E2 F4 01 F5
+
+/* Torque Loop  400 */
+3E A1 01 02 E2 90 01 91
+
+/* Torque Loop  300 */
+3E A1 01 02 E2 2C 01 2D
+
+/* Torque Loop  200 */
+3E A1 01 02 E2 C8 00 C8
+
+/* Torque Loop  100 */
+3E A1 01 02 E2 64 00 64
+
+/* Torque Loop  50 */
+3E A1 01 02 E2 32 00 32
+
+/* Torque Loop  0 */
+3E A1 01 02 E2 00 00 00
+
+
+/********************************************************************************************************************************************/
+/* Open Loop */
+
+/* Open Loop 850 */
+3E A0 01 02 E1 52 03 55
+
+/* Open Loop 768 */
+3E A0 01 02 E1 00 03 03
+
+/* Open Loop 512 */
+3E A0 01 02 E1 00 02 02
+
+/* Open Loop 256 */
+3E A0 01 02 E1 00 01 01
+
+/* Open Loop 800 */
+3E A0 01 02 E1 20 03 23
+
+/* Open Loop 700 */
+3E A0 01 02 E1 BC 02 BE
+
+/* Open Loop 600 */
+3E A0 01 02 E1 58 02 5A
+
+/* Open Loop 500 */
+3E A0 01 02 E1 F4 01 F5
+
+/* Open Loop 400 */
+3E A0 01 02 E1 90 01 91
+
+/* Open Loop 300 */
+3E A0 01 02 E1 2C 01 2D
+
+/* Open Loop 200 */
+3E A0 01 02 E1 C8 00 C8
+
+/* Open Loop 100 */
+3E A0 01 02 E1 64 00 64
+
+/* Open Loop 64 */
+3E A0 01 02 E1 40 00 40
+
+/* Open Loop 0 */
+3E A0 01 02 E1 00 00 00
+
+/********************************************************************************************************************************************/
+/* MOTOR OFF */
+3E 80 01 00 BF
+
+/* MOTOR STOP */
+3E 81 01 00 C0
+
+/* MOTOR RUN */
+3E 88 01 00 C7
+
+/* Read status 1 */
+3E 9A 01 00 D9
+
+/* Read status 2 */
+3E 9C 01 00 DB
+
+/* Read encoder data */
+3E 90 01 00 CF
+
+/* Connect */
+3E 10 01 00 4F
+
+/* Disconnect */
+3E 11 01 00 50
+
+/* Read info */
+3E 12 01 00 51
+
+/* Read motor Multi angle */
+3E 92 01 00 D1
+
+/* Read motor single angle */
+3E 94 01 00 D3
+
+/* Set Acceleration  1440 */
+3E 34 01 04 77 A0 05 00 00 A5
+
+/* Read Acceleration */
+3E 33 01 00 72
