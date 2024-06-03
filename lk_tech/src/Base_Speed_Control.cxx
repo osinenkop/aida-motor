8291a941f4ae45450d7ede74445e7a2c26c7a5b7
sinamoghimi <sinamoghimi73@gmail.com>
2024-06-03 18:55:09 +0300

    Speed Control Script created

---
+++ lk_tech/src/Base_Speed_Control.cxx
@@ -0,0 +1,48 @@
+#include "Base.hxx"
+
+auto Base::initSpeedControlCmd() -> void{
+    this -> data_.closed_loop_speed_cmd_ = {0x3E, 0xA2, this->device_id_, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    this -> client_.calculateCheckSum(this -> data_.closed_loop_speed_cmd_, 0, 3, this -> data_.closed_loop_speed_cmd_[4]);
+    this -> data_.closed_loop_speed_response_.resize(13, 0x00);
+}
+
+auto Base::closedLoopSpeedControl(const std::int32_t& value) -> void{
+    /*The computer host sends this command to control the speed of the motor with a speedControl 
+    of type int32_t corresponding to the actual speed of 0.01 DPS /LSB.*/
+
+    this -> data_.closed_loop_speed_cmd_[5] = ((value          ) & 0xFF);
+    this -> data_.closed_loop_speed_cmd_[6] = ((value >> 1     ) & 0xFF);
+    this -> data_.closed_loop_speed_cmd_[7] = ((value >> 2     ) & 0xFF);
+    this -> data_.closed_loop_speed_cmd_[8] = ((value >> 3     ) & 0xFF);
+
+    this -> client_.calculateCheckSum(this -> data_.closed_loop_speed_cmd_, 5, 8, this -> data_.closed_loop_speed_cmd_[9]);
+
+    this -> client_.send(this -> data_.closed_loop_speed_cmd_);
+    this -> client_.receive(this -> data_.closed_loop_speed_response_);
+
+    /*The motor replies to the computer host after receiving the command, and the frame data contains the following data.
+    1. Motor temperature (int8_t type, 1°C/LSB).
+    2. Torque current IQ of the motor (int16_t type, range -2048~2048, corresponding to the actual torque current range -33A ~33A). 
+    3. Motor power output value (int16_t type, range -1000~1000) 3.Motor speed (int16_t type, 1dps/LSB).
+    4. Encoder position value (uint16_t type, 14bit encoder value range 0~16383).*/
+
+    this -> temperature = this -> data_.closed_loop_speed_response_[5];
+
+    this -> torque    = (
+                        (this -> data_.closed_loop_speed_response_[6]            ) +
+                        (this -> data_.closed_loop_speed_response_[7] << 8       ) 
+                        );
+
+
+    this -> speed     = (
+                        (this -> data_.closed_loop_speed_response_[8]            ) +
+                        (this -> data_.closed_loop_speed_response_[9] << 8       ) 
+                        );
+
+
+    this -> position  = (
+                        (this -> data_.closed_loop_speed_response_[10]           ) +
+                        (this -> data_.closed_loop_speed_response_[11] << 8      ) 
+                        );
+}
+
