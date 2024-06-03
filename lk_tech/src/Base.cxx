cc92460fa6e2e53205a9994a5040d5b9971eed1c
sinamoghimi <sinamoghimi73@gmail.com>
2024-06-03 18:53:24 +0300

    removed Base::removeSpace and updated

--- lk_tech/src/Base.cxx
+++ lk_tech/src/Base.cxx
@@ -8,28 +8,20 @@ Base::~Base(){
 
 Base::Base(std::string port_address, uint8_t device_id): device_id_{device_id}, client_{PortHandler(port_address)}{
 
-    this -> response_str.resize(255);
-
     this -> initModelCmd();
     this -> initPIDCmd();
     this -> initAccelerationCmd();
     this -> initPositionCmd();
     this -> initStateCmd();
     this -> initOperationCmd();
-    this -> initTorqueCmd();
+
+    this -> initTorqueControlCmd();
+    this -> initSpeedControlCmd();
+    this -> initPositionControlCmd();
+    
 
     this -> turnOn();
 }
-
-
-auto Base::removeSpace(const std::vector<uint8_t>& arr) -> void{
-    this -> response_str.clear();
-    std::copy_if(arr.begin(), arr.end(),
-                    // std::back_inserter(this -> response_str),
-                    this -> response_str.begin(),
-                    [](uint8_t x) { return '\0' != x; });
-}
-
 
 
 
