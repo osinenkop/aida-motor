a9df56e1dfb4e295c663418f10abcf025ea5b1bf
sinamoghimi <sinamoghimi73@gmail.com>
2024-06-06 19:05:01 +0300

    Update Motor.hxx

--- lk_tech/include/Motor.hxx
+++ lk_tech/include/Motor.hxx
@@ -3,36 +3,53 @@
 
 #include <string>
 #include <cstdint>
+#include <cassert>
 #include "Base.hxx"
+#include "Data.hxx"
 
-
-struct PID{
-float position_Kp, position_Ki;
-float speed_Kp, speed_Ki;
-float torque_Kp, torque_Ki;
-};
 
 class Motor{
     public:
         Motor(std::string, std::uint8_t);
         ~Motor();
-        auto setPID() -> void;
-        auto getPID() -> void;
+        auto setPID(const std::array<std::uint8_t, 6>&&) -> void;
+        auto setPID(const std::array<std::uint8_t, 6>&) -> void;
+        auto getPID() -> const PID&;
 
-        auto setAcc() -> void;
-        auto getAcc() -> void;
+        auto setAcc(const std::int32_t&) -> void;
+        auto getAcc() -> std::int32_t&;
 
+        auto setPositionOffset(const float&) -> void;
+        auto getPosition() -> float;
         auto getState() -> void;
+        auto getCurrent() -> void;
 
-        auto SpeedControl() -> void;
-        auto TorqueControl() -> void;
-        auto PositionControl() -> void;
+        auto torqueControl(const float&) -> void;
+        auto speedControl(const float&) -> void;
+        auto positionControl(const float&, const float&) -> void;
 
         auto getProductInfo() -> const std::string&;
 
+        
 
+    private:
+        auto convertCurrent(const std::int16_t&) -> float;
 
+        template <typename In, typename Out>
+        auto convertTorque(const In&, bool&&) -> Out;
+
+        template <typename In, typename Out>
+        auto convertSpeed(const In&, bool&&) -> Out;
+
+        template <typename In, typename Out>
+        auto convertPosition(const In&, bool&&) -> Out;
+
+        auto collectTorqueSpeedPoseData() -> void;
+        auto sgn(const float&) -> bool;
+        
+    public:
         Base base;
+        PID pid;
         std::string model_name;
 
         float temperature{};
@@ -40,13 +57,22 @@ class Motor{
         float speed{};
         float acceleration{};
         float position{};
-        PID pid{};
 
         std::uint8_t error_state{};
         float voltage{};
 
+        float phase_a_current{}, phase_b_current{}, phase_c_current{};
+
         std::string port_address;
         uint8_t device_id;
+
+    private:
+    bool model_name_exist{};
+    const float LSB{5.2 / (1 << 14)};
+    const std::uint8_t reduction_ratio{8};
+    const std::uint16_t fourteen_bit_resolution{(1 << 14)};
+
+    const float current_limit{18};
 
 };
 
