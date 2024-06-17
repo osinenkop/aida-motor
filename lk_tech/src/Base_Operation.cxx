72b829699102b395ee8ce3177d958c36c1016935
sinamoghimi <sinamoghimi73@gmail.com>
2024-06-17 20:40:12 +0300

    Update Base_Operation.cxx

--- lk_tech/src/Base_Operation.cxx
+++ lk_tech/src/Base_Operation.cxx
@@ -19,33 +19,35 @@ auto Base::initOperationCmd() -> void{
 
 auto Base::shutdown() -> void{
     /*Turn off the motor and clear the motor running state and the control instruction received before.*/
-
+    if (this -> turn_on_success){
     this -> client_.send(this -> data_.shutdown_cmd_);
     this -> client_.receive(this -> data_.shutdown_response_);
 
     /*On successful attempt, the response should be the same as the command*/
     this -> shutdown_success = (this -> data_.shutdown_cmd_ == this -> data_.shutdown_response_);
-    assert(this -> shutdown_success);
+    // assert(this -> shutdown_success);
+    }
 }
 
 auto Base::stop() -> void{
     /*Stop the motor, but do not clear the motor running state and previous received control instructions.*/
+    if (this -> turn_on_success){
+        this -> client_.send(this -> data_.stop_cmd_);
+        this -> client_.receive(this -> data_.stop_response_);
 
-    this -> client_.send(this -> data_.stop_cmd_);
-    this -> client_.receive(this -> data_.stop_response_);
-
-    /*On successful attempt, the response should be the same as the command*/
-    this -> stop_success = (this -> data_.stop_cmd_ == this -> data_.stop_response_);
-    assert(this -> stop_success);
+        /*On successful attempt, the response should be the same as the command*/
+        this -> stop_success = (this -> data_.stop_cmd_ == this -> data_.stop_response_);
+        // assert(this -> stop_success);
+    }
 }
 
 auto Base::turnOn() -> void{
     /*Restore motor operation from motor stop command (control mode before restoration stop).*/
-
+    
     this -> client_.send(this -> data_.turn_on_cmd_);
     this -> client_.receive(this -> data_.turn_on_response_);
 
     /*On successful attempt, the response should be the same as the command*/
     this -> turn_on_success = (this -> data_.turn_on_cmd_ == this -> data_.turn_on_response_);
-    assert(this -> turn_on_success);
+    // assert(this -> turn_on_success);
 }
