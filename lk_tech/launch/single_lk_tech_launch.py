bfce34ccfea89745e7c24244fbc65f0ba5b9e7bf
sinamoghimi <sinamoghimi73@gmail.com>
2024-06-25 18:14:50 +0300

    Update single_lk_tech_launch.py

--- lk_tech/launch/single_lk_tech_launch.py
+++ lk_tech/launch/single_lk_tech_launch.py
@@ -8,12 +8,29 @@ from launch.substitutions import LaunchConfiguration
 import os, yaml, sys, json, re
 
 
-data = {"id": "1",
-        "initial_pose": [0., 1.5, 0.]}
+port_list = os.popen("ls /dev | grep ttyUSB").read().split('\n')[:-1]
+port_list = ["/dev/" + port for port in port_list]
 
+
+
+data = {"id": "3",
+        "initial_pose": [0.5, -0.5, 0.],
+        "initial_orientation": [0., 1.57, 0],
+        "port_list": port_list}
+
+motor_name = "motor_" + data["id"]
 data["id"] = data["id"] + "_"
+
+
 params = [{key: data[key]} for key in data.keys()]
 
 def generate_launch_description():
-    return LaunchDescription([Node(package="lk_tech", executable="lk_tech_motor", name="Motor", namespace = "LK_TECH", parameters=[data], output='screen')]
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments = [str(data["initial_pose"][0]), str(data["initial_pose"][1]), str(data["initial_pose"][2]), str(data["initial_orientation"][0]), str(data["initial_orientation"][1]), str(data["initial_orientation"][2]), "1", "robot", motor_name]
+        ),
+        Node(package="lk_tech", executable="lk_tech_motor", name="Motor", namespace = "LK_TECH", parameters=[data], output='screen')
+        ]
 )
